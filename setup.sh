45bc74a1c5b9175fc5fe7c3183a6deebdc1842a0
devsimsek <mtnsmsk61@icloud.com>
2021-05-08 01:43:20 +0300

    Added Setup.sh For v1.3

---
+++ setup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# Copyright devsimsek
+# https://github.com/devsimsek/phpRest
+latest_zipball=https://api.github.com/repos/devsimsek/phpRest/zipball
+installer_log=phpRest_installer.log
+defdir=phpRestv1.2
+version="1.3"
+ghdir=ddevsimsek-phpRest-6bdac0e
+spinner() {
+  local pid=$!
+  local delay=0.75
+  local spinstr='|/-\'
+  while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+    local temp=${spinstr#?}
+    printf "$1 [%c]  " "$spinstr"
+    local spinstr=$temp${spinstr%"$temp"}
+    sleep $delay
+    printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
+  done
+  printf "Done...    \b\b\b\b"
+}
+clear
+echo "              __                            __  "
+echo "   ____ ___  / /_____  _________ ___  _____/ /__"
+echo "  / __ ´__ \/ __/ __ \/ ___/ __ ´__ \/ ___/ //_/"
+echo " / / / / / / /_/ / / (__  ) / / / / (__  ) ,<   "
+echo "/_/ /_/ /_/\__/_/ /_/____/_/ /_/ /_/____/_/|_|  "
+echo "Welcome To the svcs phpRest " version " installer"
+echo "Warning! This installer is not done yet! Please do not use it!"
+echo $(date +'%m/%d/%Y:%R') ": ---------------Start Installer Log---------------" >>$installer_log
+echo $(date +'%m/%d/%Y:%R') ": Checking" $defdir "Directory" >>$installer_log
+if [ ! -d "$defdir" ]; then
+  echo $(date +'%m/%d/%Y:%R') ": " $defdir "Directory Not Found. Creating..." >>$installer_log
+  mkdir $defdir
+  cd $defdir
+  echo $(date +'%m/%d/%Y:%R') ": "$defdir "Directory Created. Starting Git Clone Process..." >>$installer_log
+  (wget -q -c $latest_zipball -O temp.zip) &
+  spinner "Downloading phpRest..."
+  (unzip temp.zip && rm temp.zip) &
+  spinner "Setting Up Files..."
+  cd $ghdir
+  mv * ../
+  cd ../
+  rm -rf $ghdir
+  cat >".htaccess" <<-"EOF"
+DirectoryIndex index.php
+
+# enable apache rewrite engine
+RewriteEngine on
+
+# set your rewrite base
+# Edit this in your init method too if you script lives in a subfolder
+RewriteBase /
+
+# Deliver the folder or file directly if it exists on the server
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+
+# Push every request to index.php
+RewriteRule ^(.*)$ index.php [QSA]
+EOF
+  cd ../
+  chmod +x $defdir
+  echo $(date +'%m/%d/%Y:%R') ": Installation Finished. User Can Start Application With cd $defdir && php -S localhost:6107 in terminal..." >>$installer_log
+  echo $(date +'%m/%d/%Y:%R') ": Installation Finished. You Can Start Application With cd $defdir && php -S localhost:6107 in terminal..."
+else
+  echo "Files Exists. Killing Connection..."
+fi
