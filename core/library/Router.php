6311d9c75f62487a50a4ce1193c497d0ea6bc010
devsimsek <mtnsmsk61@icloud.com>
2021-05-08 01:30:03 +0300

    Added v1.3 Router Class

--- core/library/Router.php
+++ core/library/Router.php
@@ -3,6 +3,7 @@ defined('BASE_DIR') or exit('Sorry. No direct access allowed here!');
 
 class Router
 {
+
     /**
      * Loader object to use.
      * @var
@@ -10,69 +11,143 @@ class Router
     public $load;
 
     /**
-     * The request we're working with.
-     *
-     * @var string
-     */
-    public $request;
-
-    /**
      * The $routes array will contain our URI's and callbacks.
      * @var array
      */
-    public $routes = [];
+    private static $routes = array();
 
     /**
-     * For this example, the constructor will be responsible
-     * for parsing the request.
-     *
-     * @param array $request
+     * The variable to store if the path found or not.
      */
-    public function __construct(array $request)
-    {
-        /**
-         * This is a very (VERY) simple example of parsing
-         * the request. We use the $_SERVER superglobal to
-         * grab the URI.
-         */
-        $this->request = basename($request['REQUEST_URI']);
+    private static $pathNotFound = null;
 
+    /**
+     * The variable to store if the method is not allowed or not.
+     */
+    private static $methodNotAllowed = null;
+
+    /**
+     * Router constructor.
+     *
+     * $this->load is main loader object.
+     */
+    public function __construct()
+    {
         load_class('Loader', 'library');
         $this->load = new Loader();
     }
 
+
     /**
-     * Add a route and callback to our $routes array.
-     *
-     * @param string $uri
-     * @param Callable $fn
+     * @param $expression
+     * @param $function
+     * @param string $method
      */
-    public function addRoute(string $uri, \Closure $fn): void
+    public static function add($expression, $function, $method = 'get')
     {
-        $this->routes[$uri] = $fn;
+        array_push(self::$routes, array(
+            'expression' => $expression,
+            'function' => $function,
+            'method' => $method
+        ));
     }
 
     /**
-     * Determine is the requested route exists in our
-     * routes array.
-     *
-     * @param string $uri
-     * @return boolean
+     * @param $function
      */
-    public function hasRoute(string $uri): bool
+    public static function pathNotFound($function)
     {
-        return array_key_exists($uri, $this->routes);
+        self::$pathNotFound = $function;
     }
 
     /**
-     * Run the router.
-     *
-     * @return mixed
+     * @param $function
      */
-    public function ignite()
+    public static function methodNotAllowed($function)
     {
-        if ($this->hasRoute($this->request)) {
-            $this->routes[$this->request]->call($this);
+        self::$methodNotAllowed = $function;
+    }
+
+    /**
+     * Start Routing Process
+     * @param string $basepath
+     */
+    public static function ignite($basepath = '/')
+    {
+
+        // Parse current url
+        $parsed_url = parse_url($_SERVER['REQUEST_URI']);//Parse Uri
+
+        if (isset($parsed_url['path'])) {
+            $path = $parsed_url['path'];
+        } else {
+            $path = '/';
         }
+
+        // Get current request method
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $path_match_found = false;
+
+        $route_match_found = false;
+
+        foreach (self::$routes as $route) {
+
+            // If the method matches check the path
+
+            // Add basepath to matching string
+            if ($basepath != '' && $basepath != '/') {
+                $route['expression'] = '(' . $basepath . ')' . $route['expression'];
+            }
+
+            // Add 'find string start' automatically
+            $route['expression'] = '^' . $route['expression'];
+
+            // Add 'find string end' automatically
+            $route['expression'] = $route['expression'] . '$';
+
+            // Check path match
+            if (preg_match('#' . $route['expression'] . '#', $path, $matches)) {
+
+                $path_match_found = true;
+
+                // Check method match
+                if (strtolower($method) == strtolower($route['method'])) {
+
+                    array_shift($matches);// Always remove first element. This contains the whole string
+
+                    if ($basepath != '' && $basepath != '/') {
+                        array_shift($matches);// Remove basepath
+                    }
+
+                    call_user_func_array($route['function'], $matches);
+
+                    $route_match_found = true;
+
+                    // Do not check other routes
+                    break;
+                }
+            }
+        }
+
+        // No matching route was found
+        if (!$route_match_found) {
+
+            // But a matching path exists
+            if ($path_match_found) {
+                header("HTTP/1.0 405 Method Not Allowed");
+                if (self::$methodNotAllowed) {
+                    call_user_func_array(self::$methodNotAllowed, array($path, $method));
+                }
+            } else {
+                header("HTTP/1.0 404 Not Found");
+                if (self::$pathNotFound) {
+                    call_user_func_array(self::$pathNotFound, array($path));
+                }
+            }
+
+        }
+
     }
+
 }
